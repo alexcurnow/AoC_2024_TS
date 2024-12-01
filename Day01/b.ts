7f9cdee719922e4922a9ff45c27935ca9bf57c42
Alex Curnow <alexcurnow@gmail.com>
2024-12-01 11:49:50 -0600

    Day 01 part duex complete.

--- Day01/b.ts
+++ Day01/b.ts
@@ -11,16 +11,41 @@ export async function readFileAsync(filePath: string): Promise<string> {
 }
 
 async function main() {
-  let input: string;
 
-  input = await readFileAsync("./input.txt");
+  const input: string = await readFileAsync("./input.txt");
 
+  // be lazy and don't change anything with parsing and sorting shit...
   const parsedInput: string[] = input.split("\n").flatMap(pair => pair.split(" ").filter(s => s));
   const left: number[] = parsedInput.map(s => parseInt(s)).filter((_, idx) => idx % 2 === 0).sort();
   const right: number[] = parsedInput.map(s => parseInt(s)).filter((_, idx) => idx % 2 !== 0).sort();
-  const result: number = left.map((value, idx) => Math.abs(value - right[idx])).reduce((prev, curr) => prev + curr);
 
-  console.log(result);
+  // init a new dictionary aka Map<T, T>
+  const dict = new Map<number, number>();
+
+  // for each number in the right collection...
+  right.forEach(num => {
+    //... if it's in the left collection...
+    if (left.includes(num)) {
+      //... get its value from the dictionary or return 0...
+      const value = dict.get(num) ?? 0;
+      // ... set (if it's initial) or update the key value pair by incrementing the value at that key.
+      dict.set(num, value + 1);
+    }
+  });
+
+  // init an empty numbers array
+  let results: number[] = [];
+
+  // for each key value pair in the dictionary...
+  dict.forEach((value, key) => {
+    //... push the result of multiplying the value and key together
+    results.push(value * key);
+  });
+
+  //... use the reduce method to sum all those values together
+  const finalResult = results.reduce((prev, curr) => prev + curr);
+
+  console.log(finalResult);
 }
 
 main();
